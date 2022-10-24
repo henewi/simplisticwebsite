89be01257b1cf02b79981ff333a2e5665f6867ef
henewi <hpdunn2002@gmail.com>
2022-10-24 21:34:00 +0100

    font added

--- javascript.js
+++ javascript.js
@@ -3,8 +3,6 @@ function setTheme() {
     document.documentElement;
     const newTheme = root.className === 'dark' ? 'light': 'dark';
     root.className = newTheme;
-
-    document.querySelector('.theme-name').textContent = newTheme;
 }
 
 document.querySelector('.theme-toggle').addEventListener('click', setTheme)
