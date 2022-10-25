9d9de1dd3d7cb8848ab292aa32ee879a1c76369e
henewi <hpdunn2002@gmail.com>
2022-10-25 22:14:29 +0100

    testing button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-function setTheme() {
-    const root =
-    document.documentElement;
-    const newTheme = root.className === 'dark' ? 'light': 'dark';
-    root.className = newTheme;
-}
+// function setTheme() {
+//     const root =
+//     document.documentElement;
+//     const newTheme = root.className === 'dark' ? 'light': 'dark';
+//     root.className = newTheme;
+// }
 
-document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+// document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+
+let modeToggle = document.querySelector('.mode-tog');
+let darkMode = document.querySelector('.dark-mode');
+modeToggle.addEventListener('click', () =>{
+    darkMode.classList.toggle('active');
+    modeToggle.classList.toggle('active');
+})
